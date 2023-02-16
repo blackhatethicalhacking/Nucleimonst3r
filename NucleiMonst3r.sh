045a450e41d58c79a0e142f8e91d440d0046ddee
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-16 15:38:13 +0200

    Update NucleiMonst3r.sh

--- NucleiMonst3r.sh
+++ NucleiMonst3r.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-
+curl --silent "https://raw.githubusercontent.com/blackhatethicalhacking/Subdomain_Bruteforce_bheh/main/ascii.sh" | lolcat
+echo ""
+# Generate a random Sun Tzu quote for offensive security
+# Array of Sun Tzu quotes
+quotes=("The supreme art of war is to subdue the enemy without fighting." "All warfare is based on deception." "He who knows when he can fight and when he cannot, will be victorious." "The whole secret lies in confusing the enemy, so that he cannot fathom our real intent." "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.")
+# Get a random quote from the array
+random_quote=${quotes[$RANDOM % ${#quotes[@]}]}
+# Print the quote
+echo "Offensive Security Tip: $random_quote - Sun Tzu" | lolcat
+sleep 1
+echo "MEANS, IT'S ☕ 1337 ⚡ TIME, 369 ☯ " | lolcat
+sleep 1
+figlet -w 80 -f small NucleiMonst3r | lolcat
+echo ""
+echo "[YOUR ARE USING NucleiMonst3r.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
+sleep 1
+#check if the user is connected to the internet
+tput bold;echo "CHECKING IF YOU ARE CONNECTED TO THE INTERNET!" | lolcat
+# Check connection
+wget -q --spider https://google.com
+if [ $? -ne 0 ];then
+    echo "++++ CONNECT TO THE INTERNET BEFORE RUNNING NucleiMonst3r.sh!" | lolcat
+    exit 1
+fi
+tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
 # get domain name from user input
-echo "Enter website URL:"
+echo "Enter Domain:"
 read website
 
 # create a directory with the domain name of the website
@@ -31,7 +55,7 @@ echo "5. file"
 echo "6. miscellaneous"
 echo "7. misconfiguration"
 echo "8. technologies"
-echo "9. All Templates (Takes Hours)"
+echo "9. All Templates AKA Hail Mary (Takes Hours)"
 echo "Enter the numbers separated by commas:"
 read templates
 
