f136b2d1c7099766bf865cc500bcfad18c00e64f
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-17 14:19:48 +0200

    Update NucleiMonst3r.sh

--- NucleiMonst3r.sh
+++ NucleiMonst3r.sh
@@ -24,34 +24,6 @@ if [ $? -ne 0 ];then
     exit 1
 fi
 tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
-# Matrix effect
-echo "Entering the Matrix for 5 seconds in:" | toilet --metal -f term -F border
-sleep 1
-echo "3" | toilet --gay -f term -F border
-sleep 1
-echo "2" | toilet --metal -f term -F border
-sleep 1
-echo "1" | toilet --gay -f term -F border
-sleep 1
-
-R='\033[0;31m'
-G='\033[0;32m'
-Y='\033[1;33m'
-B='\033[0;34m'
-P='\033[0;35m'
-C='\033[0;36m'
-W='\033[1;37m'
-
-for ((i=0; i<5; i++)); do
-    echo -ne "${R}10 ${G}01 ${Y}11 ${B}00 ${P}01 ${C}10 ${W}00 ${G}11 ${P}01 ${B}10 ${Y}11 ${C}00\r"
-    sleep 0.2
-    echo -ne "${R}01 ${G}10 ${Y}00 ${B}11 ${P}10 ${C}01 ${W}11 ${G}00 ${P}10 ${B}01 ${Y}00 ${C}11\r"
-    sleep 0.2
-    echo -ne "${R}11 ${G}00 ${Y}10 ${B}01 ${P}00 ${C}11 ${W}01 ${G}10 ${P}00 ${B}11 ${Y}10 ${C}01\r"
-    sleep 0.2
-    echo -ne "${R}00 ${G}11 ${Y}01 ${B}10 ${P}11 ${C}00 ${W}10 ${G}01 ${P}11 ${B}00 ${Y}01 ${C}10\r"
-    sleep 0.2
-done
 # get domain name from user input
 echo "Enter Domain:"
 read domain
@@ -119,4 +91,33 @@ if [[ $templates == *"9"* ]]; then
 fi
 
 echo "Starting Nuclei scan with the selected templates..."
-cat $domain/httpx_output.txt | nuclei -stats -si 100 $t_args -o $domain/nuclei_results_for_$domain.txt 
+cat $domain/httpx_output.txt | nuclei -stats -si 100 $t_args -o $domain/nuclei_results_for_$domain.txt
+# Matrix effect
+echo "Exiting the Matrix for 5 seconds in:" | toilet --metal -f term -F border
+sleep 1
+echo "3" | toilet --gay -f term -F border
+sleep 1
+echo "2" | toilet --metal -f term -F border
+sleep 1
+echo "1" | toilet --gay -f term -F border
+sleep 1
+
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[1;33m'
+B='\033[0;34m'
+P='\033[0;35m'
+C='\033[0;36m'
+W='\033[1;37m'
+
+for ((i=0; i<5; i++)); do
+    echo -ne "${R}10 ${G}01 ${Y}11 ${B}00 ${P}01 ${C}10 ${W}00 ${G}11 ${P}01 ${B}10 ${Y}11 ${C}00\r"
+    sleep 0.2
+    echo -ne "${R}01 ${G}10 ${Y}00 ${B}11 ${P}10 ${C}01 ${W}11 ${G}00 ${P}10 ${B}01 ${Y}00 ${C}11\r"
+    sleep 0.2
+    echo -ne "${R}11 ${G}00 ${Y}10 ${B}01 ${P}00 ${C}11 ${W}01 ${G}10 ${P}00 ${B}11 ${Y}10 ${C}01\r"
+    sleep 0.2
+    echo -ne "${R}00 ${G}11 ${Y}01 ${B}10 ${P}11 ${C}00 ${W}10 ${G}01 ${P}11 ${B}00 ${Y}01 ${C}10\r"
+    sleep 0.2
+done
+echo -e "\033[2J\033[?25h" # reset screen
